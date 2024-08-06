9c3850211fd0a4f21f24eeac426d1d8c99854e94
ahmed wagih <wagiha498@gmail.com>
2024-08-06 12:55:10 +0300

    change base-URL

---
+++ assets/hooks-S2a1xkub.js
@@ -0,0 +1,1 @@
+import{r as n}from"./index-bhfotHN7.js";import{u as o}from"./MarkerIcon-Qt1AfLDv.js";function r(){return o().map}function p(e){const t=r();return n.useEffect(function(){return t.on(e),function(){t.off(e)}},[t,e]),t}export{r as a,p as u};
---
+++ assets/hooks-SMTgv0eb.js
@@ -0,0 +1,1 @@
+import{K as s,M as e}from"./index-bhfotHN7.js";const t=s,a=e;export{t as a,a as u};
